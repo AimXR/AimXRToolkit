ed6d08dc749583f9f7bd07f9f34c26222f643655
toxicbloud <fanta.r55000@gmail.com>
2023-06-14 14:47:04 +0200

    begin Translate component

--- aimxr-toolkit-csharp/component.lua
+++ aimxr-toolkit-csharp/component.lua
@@ -12,16 +12,14 @@
 -- 
 -- You should have received a copy of the GNU Lesser General Public License
 -- along with aimxr-toolkit-csharp. If not, see <http://www.gnu.org/licenses/>.
-
 -- Action function of a hypothetical home button on a machine
-
-function createComponent(name,action)
+function createComponent(name, action)
     _G[name] = {}
     if action ~= nil then
         _G[name].Action = action
     else
         _G[name].Action = function()
-        print(name .. " interacted")
+            print(name .. " interacted")
         end
     end
 end
@@ -31,7 +29,7 @@ _G["axisZ"].Home = function()
     print("Axis Z is homing")
 end
 
-createComponent("homeBtn",function()
+createComponent("homeBtn", function()
     print("Home button pressed")
     _G["axisZ"].Home()
 end)
@@ -41,3 +39,15 @@ createComponent("startBtn")
 -- emulate an action
 _G["homeBtn"].Action()
 _G["startBtn"].Action()
+
+function OnPressed()
+    for i = 1, 10 do
+        for j = 1, 10 do
+            for k = 1, 10 do
+                _G['color_smoke_power'].interactable.SetColor('#' .. i .. j .. k)
+            end
+        end
+    end
+end
+
+
