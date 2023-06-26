92ed06800d2cd6c676ec1ba901e3ec137e245c2d
FurWaz <fur.waz06@gmail.com>
2023-06-26 12:50:18 +0200

    Added artifact to instance correspondance

--- Runtime/Scripts/Manager/WorkPlaceManager.cs
+++ Runtime/Scripts/Manager/WorkPlaceManager.cs
@@ -26,11 +26,15 @@ namespace AimXRToolkit.Managers
         [SerializeField]
         public LoaderSource loaderSource = null!;
         private Models.Workplace _workplace = null!;
-        private Dictionary<int, ArtifactManager> _artifacts = null!;
+        private Dictionary<int, ArtifactManager> _artifacts = null;
+        private Dictionary<int, ArtifactManager> _artifactInstances = null;
+
         public void Start()
         {
             _artifacts = new Dictionary<int, ArtifactManager>();
+            _artifactInstances = new Dictionary<int, ArtifactManager>();
         }
+
         public async Task Spawn()
         {
             foreach (Models.ArtifactInstance instance in _workplace.GetArtifacts())
@@ -64,14 +68,17 @@ namespace AimXRToolkit.Managers
         {
             _workplace = workplace;
         }
+
         public Models.Workplace GetWorkplace()
         {
             return _workplace;
         }
+
         public async Task<List<Models.Activity>> GetCompatibleActivitiesAsync()
         {
             return await DataManager.GetInstance().GetCompatibleActivitiesAsync(_workplace.GetId());
         }
+
         /// <summary>
         /// Get instanciated artifacts in the workplace by instance id
         /// </summary>
@@ -79,6 +86,70 @@ namespace AimXRToolkit.Managers
         public Dictionary<int, ArtifactManager> GetArtifacts()
         {
             return _artifacts;
+        }
+
+        /// <summary>
+        /// Returns the corresponding artifact instance in the workplace for a given artifact id
+        /// If set using the setArtifactInstance() method, the matching artifact instance will be returned.
+        /// Else, the first matching artifact instance in workplace will be returned.
+        /// </summary>
+        /// <param name="artifactId">The requested artifact id</param>
+        /// <param name="artifact">The corresponding artifact instance manager</param>
+        /// <returns>Boolean indicating if search was successfull or not</returns>
+        public bool tryGetArtifactInstance(int artifactId, out ArtifactManager artifact)
+        {
+            // try to find a defined matching instance in the workplace
+            if (_artifactInstances.TryGetValue(artifactId, out artifact))
+            {
+                return true;
+            }
+
+            // if not found, return the first artifact in the workplace
+            foreach (var art in _artifacts.Values)
+            {
+                if (art.GetArtifact().GetId() == artifactId)
+                {
+                    artifact = art;
+                    return true;
+                }
+            }
+
+            // if not found, indicate error
+            return false;
+        }
+
+        /// <summary>
+        /// Sets a matching artifact instance for a given artifact Id
+        /// </summary>
+        /// <param name="artifactId">The targeted artifact id</param>
+        /// <param name="instance">The corresponding artifact instance manager</param>
+        /// <returns>Boolean indicating if the correspondance has been set successfully</returns>
+        public bool setArtifactInstance(int artifactId, ArtifactManager instance)
+        {
+            // artifact not in workplace (instance not found)
+            if (!_artifacts.ContainsKey(instance.GetInstanceID())) return false;
+
+            // if artifact matching instance is already defined, replace it
+            if (_artifactInstances.ContainsKey(artifactId))
+                _artifactInstances.Remove(artifactId);
+            _artifactInstances.Add(artifactId, instance);
+
+            // success
+            return true;
+        }
+
+        /// <summary>
+        /// Sets a matching artifact instance for a given artifact Id
+        /// </summary>
+        /// <param name="artifactId">The targeted artifact id</param>
+        /// <param name="instanceId">The corresponding artifact instance id</param>
+        /// <returns>Boolean indicating if the correspondance has been set successfully</returns>
+        public bool setArtifactInstance(int artifactId, int instanceId)
+        {
+            // artifact not in workplace (instance not found)
+            if (!_artifacts.ContainsKey(instanceId)) return false;
+
+            return setArtifactInstance(artifactId, _artifacts[instanceId]);
         }
     }
 }
